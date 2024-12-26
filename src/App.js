b3e712512579307aeff2c170a7db28baabac024d
ankitpastor <pastorankit18@gmail.com>
2024-12-26 18:52:20 -0500

    FAQ application using react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import FaqComp from "./FaqComp";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Frequently Asked Questions</h2>
+      <FaqComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FaqItems.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+const FaqItems = ({ faq, index }) => {
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    if (index === 0) {
+      setIsShow(true);
+    }
+  }, []);
+
+  const handleClick = () => {
+    setIsShow((isShow) => !isShow);
+  };
+  console.log(faq);
+  return (
+    <div className="faq-box">
+      <div className="que" onClick={handleClick}>
+        <button className={isShow ? "arrow" : ""}>></button>
+        <div>{faq.question}</div>
+      </div>
+      {isShow && <div className="ans">{faq.answer}</div>}
+    </div>
+  );
+};
+export default FaqItems;
